0707ee5c5af0c14f497ba6fb73c835a87ac38b73
JeanCarloCorso <jeancarlocorso@hotmail.com>
2018-01-03 20:31:18 -0200

    Arquivo não funcional

--- hana_bot.py
+++ hana_bot.py
@@ -1,14 +1,20 @@
+# -*- coding: utf-8 -*-
 from chatterbot.trainers import ListTrainer 
 from chatterbot import ChatBot 
 
 bot = ChatBot('Hana')
 
-conversa = ['Oi', 'Ola', 'Tudo bem com voce?', 'Eu estou bem, Obrigado']
+conversa = ['Oi', 'Olá', 'Tudo bem com você?', 'Eu estou bem, Obrigado']
+arquivo = open("exemplo.txt", 'r')
 
 bot.set_trainer(ListTrainer)
 bot.train(conversa)
+bot.train(arquivo)
 
 while True:
-    quest = input("Voce: ")
-    resposta = bot.get_response(quest)
-    print ("Hana: ", resposta)
+    questao = input("Você: ")
+    resposta = bot.get_response(questao)
+    if float(resposta.confidence) > 0.5:
+        print ('Hana: ', resposta)
+    else:
+        print ('Eu não sei :(')
