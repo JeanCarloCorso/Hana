fed86688e5932a865d41aae4f74e3d167094cb08
JeanCarloCorso <jeancarlocorso@hotmail.com>
2018-01-03 23:41:17 -0200

    Novas falas

--- hana_bot.py
+++ hana_bot.py
@@ -4,12 +4,19 @@ from chatterbot import ChatBot
 
 bot = ChatBot('Hana')
 
-conversa = ['Oi', 'Olá', 'Tudo bem com você?', 'Eu estou bem, Obrigado']
-arquivo = open("exemplo.txt", 'r')
+conversa = ['Oi', 'Olá', 'Tudo bem com você?', 'Eu estou bem, Obrigado', 'Obrigado', 'Não tem de que :)']
+conversa2 = {
+    'Qual é o seu nome?', 'Eu me chamo Hana', 
+    'É um belo nome!', 'Obrigada',  
+    'Quem é você?', 'Eu sou a Hana, uma inteligência artificial criada para tornar as férias do meu criador um pouco mais divertida.',
+    'Quem é o seu pai?', 'Meu pai é um jovem cientista da computação chamado Jean Carlo Corso',
+    'Quantos anos você tem?', 'Eu nasci em 03/01/2018',
+    'Porque você foi criada?', 'Para divertir as pessoas!'
+    }
 
 bot.set_trainer(ListTrainer)
 bot.train(conversa)
-bot.train(arquivo)
+bot.train(conversa2)
 
 while True:
     questao = input("Você: ")
